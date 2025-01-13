32e5e757918dd7110ab17a43086ee91a55ba5547
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2025-01-13 15:28:45 +0700

    handle cors

--- backend_patient_app/index.js
+++ backend_patient_app/index.js
@@ -32,7 +32,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://127.0.0.1:3000",
-      "https://34.135.114.112:3000",
+      "http://34.135.114.112:3000",
     ],
     exposedHeaders: "Authorization",
     methods: ["GET", "PUT", "POST", "DELETE"],
