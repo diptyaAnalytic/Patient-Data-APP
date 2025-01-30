2e9746196e6e7f68d90225592c9e3b5426e5882e
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2025-01-30 12:02:00 +0700

    .

--- backend_patient_app/config/redis.js
+++ backend_patient_app/config/redis.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 const redis = require("redis");
 
-console.log(process.env.PASSWORD_REDIS);
-console.log(process.env.PORT_REDIS);
 
 const client = redis.createClient({
   socket: {
     host: "127.0.0.1",
-    port: process.env.PORT_REDIS,
-    password: process.env.PASSWORD_REDIS,
+    port: 6379,
+    password: "sandhiguna",
   },
 });
 
